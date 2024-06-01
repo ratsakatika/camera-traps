c6bacddaa70dfb8c8f3d7880015d08e96de841a1
Tom-Ratsakatika <trr26@cam.ac.uk>
2024-06-01 17:45:46 +0100

    Major progress on final alert system

--- scripts/alert_system_utils.py
+++ scripts/alert_system_utils.py
@@ -13,8 +13,6 @@ from PIL import Image
 import ast
 from IPython.display import display
 from megadetector.visualization import visualization_utils as vis_utils
-import io
-from PIL import Image
 
 def set_device():
     return 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -42,15 +40,15 @@ def detector(df, model, images, DETECTION_THRESHOLD):
         result = model.generate_detections_one_image(processed_image)
         detections_above_threshold = [d for d in result['detections'] if d['conf'] > DETECTION_THRESHOLD]
 
-        detection_boxes = ", ".join(str(d['bbox']) for d in detections_above_threshold)
-        detection_classes = ", ".join(str(d['category']) for d in detections_above_threshold)
-        detection_confidences = ", ".join(str(d['conf']) for d in detections_above_threshold)
+        detection_boxes = [d['bbox'] for d in detections_above_threshold]
+        detection_classes = [d['category'] for d in detections_above_threshold]
+        detection_confidences = [d['conf'] for d in detections_above_threshold]
 
         animal_count = sum(1 for d in detections_above_threshold if d['category'] == '1')
         human_count = sum(1 for d in detections_above_threshold if d['category'] == '2')
         vehicle_count = sum(1 for d in detections_above_threshold if d['category'] == '3')
 
-        print(f"Image {i+1}: Animal Count = {animal_count}, Human Count = {human_count}, Vehicle Count = {vehicle_count}")
+        print(f"{current_time()} | Image {i+1}: Animal Count = {animal_count}, Human Count = {human_count}, Vehicle Count = {vehicle_count}")
 
 
         # Update the respective row in the DataFrame
@@ -88,24 +86,26 @@ class classifier:
             return self.animal_classes[top_class.item()], top_p.item()
 
 
-def batch_classification(df, images, classifier_model):
+def batch_classification(df, classifier_model, images, CLASSIFICATION_THRESHOLD):
     num_images = len(images)
+    df_length = len(df)
 
     for i, image in enumerate(images):  # Use the already opened images
 
         species_list = []
-        species_species_conf_list = []
+        species_confidence_list = []
 
-        detection_boxes = ast.literal_eval(df['Detection Boxes'][i])  # Convert the string to list
-        detection_boxes = [detection_boxes] if isinstance(detection_boxes[0], float) else detection_boxes  # Ensure it's a list of lists
+        detection_boxes = df['Detection Boxes'][df_length-num_images + i]
 
-        detection_classes = df['Detection Classes'][i].split(', ')  # Split the string into a list
-        detection_conf = df['Detection Confidences'][i].split(', ')  # Split the string into a list
+        if detection_boxes == "":
+            continue
+
+        detection_classes = df['Detection Classes'][df_length-num_images + i]
+        detection_conf = df['Detection Confidences'][df_length-num_images + i]
 
         for j, bbox in enumerate(detection_boxes):
-
+            
             if detection_classes[j] == '1':  # Only classify if an animal
-
 
                 left, top, width, height = bbox  # Unpack the bounding box
 
@@ -124,15 +124,47 @@ def batch_classification(df, images, classifier_model):
                 cropped_image = image.crop((left_resized, top_resized, right_resized, bottom_resized))
 
                 species, species_conf = classifier_model.predict(cropped_image)
-                species_list.append(species)
-                species_species_conf_list.append(species_conf)
 
-                print(f"Image {i+1}, Detection {j+1} ({detection_conf[j]} confidence), Species: {species} ({species_conf * 100:.2f}% confidence)")
-                display(cropped_image)
+                if species_conf >= CLASSIFICATION_THRESHOLD:
+                    
+                    species_list.append(species)
+                    species_confidence_list.append(species_conf)
 
+                    print(f"{current_time()} | Image {i+1}, Detection {j+1} ({detection_conf[j] * 100:.2f}% confidence), Species: {species} ({species_conf * 100:.2f}% confidence)")
+                
+                else:
+                    
+                    print(f"{current_time()} | Image {i+1}, Detection {j+1} ({detection_conf[j] * 100:.2f}% confidence), Species: {species} ({species_conf * 100:.2f}% confidence). EXCLUDED: Below Species Confidence Threshold ({CLASSIFICATION_THRESHOLD * 100:.2f}%)")
 
-        df.at[df.index[-num_images + i], 'Species Classes'] = ", ".join(species_list)
-        df.at[df.index[-num_images + i], 'Species Confidences'] = ", ".join(map(str, species_species_conf_list))
+                
+                # display(cropped_image)
+
+        if species_list:
+
+            primary_species = max(set(species_list), key=species_list.count)
+            wild_boar_count = species_list.count('Wild Boar')
+            bear_count = species_list.count('Bear')
+
+        elif df['Human Count'][df_length-num_images + i] > 0:
+            primary_species = "Human"
+            wild_boar_count = 0
+            bear_count = 0
+
+        elif df['Vehicle Count'][df_length-num_images + i] > 0:
+            primary_species = "Vehicle"
+            wild_boar_count = 0
+            bear_count = 0
+
+        else:
+            primary_species = "Empty"
+            wild_boar_count = 0
+            bear_count = 0
+
+        df.at[df.index[-num_images + i], 'Species Classes'] = species_list
+        df.at[df.index[-num_images + i], 'Species Confidences'] = species_confidence_list
+        df.at[df.index[-num_images + i], 'Primary Species'] = primary_species
+        df.at[df.index[-num_images + i], 'Wild Boar Count'] = wild_boar_count
+        df.at[df.index[-num_images + i], 'Bear Count'] = bear_count
 
     return df
 
@@ -240,14 +272,15 @@ def download_image_from_url(url):
         response.raise_for_status()
         return Image.open(io.BytesIO(response.content))
     except requests.RequestException as e:
-        print(f"Error downloading image from {url}: {str(e)}")
+        print(f"{current_time()} | Error downloading image from {url}: {str(e)}")
         return None
 
 
 def extract_camera_id(subject, body):
     # Extract camera ID from subject
-    camera_id = None
-    if subject:
+    camera_id = "Unknown"
+
+    if subject and '_' in subject:
         camera_id = subject.split('_')[-1]
 
     # Extract camera ID from body
@@ -269,7 +302,7 @@ def extract_date_time_from_image(image):
                 time_obj = datetime.strptime(time_str, '%H:%M:%S')
                 return date_obj.strftime('%Y-%m-%d'), time_obj.strftime('%H:%M:%S')
     except Exception as e:
-        print(f"Error extracting date and time from image: {e}")
+        print(f"{current_time()} | Error extracting date and time from image: {e}")
     return None, None
 
 
@@ -363,7 +396,7 @@ def extract_and_update_camera_info(CAMERA_LOCATIONS_PATH, camera_id, battery=Non
     camera_row = df[df['Camera ID'] == camera_id]
 
     if camera_row.empty:
-        print(f"No camera found with ID {camera_id}")
+        print(f"{current_time()} | No camera found with ID: {camera_id}")
         return None, None, None, None, None, None
 
     # Extract the details
@@ -390,9 +423,20 @@ def extract_and_update_camera_info(CAMERA_LOCATIONS_PATH, camera_id, battery=Non
 
 
 def update_camera_data_dataframe(df, images_count, camera_id, camera_make, img_date, img_time, temp_deg_c, battery, sd_memory, location, gps, map_url):
+    
+    if camera_make == None:
+        camera_id = "Unknown"
+        camera_make = "Unknown"
+        location = "Unknown"
+        gps = "Unknown"
+        battery = "Unknown"
+        sd_memory = "Unknown"
+        map_url = "Unknown"
+        temp_deg_c = "Unknown"
+    
     # Determine the new Sequence ID by adding 1 to the last existing Sequence ID
     last_sequence_id = df['Sequence ID'].max() if not df.empty else 100000
-    new_sequence_id = last_sequence_id + 1
+    new_sequence_id = int(last_sequence_id + 1)
 
     # Creating a list for the sequence column
     sequence = [new_sequence_id] * images_count
@@ -423,14 +467,195 @@ def update_camera_data_dataframe(df, images_count, camera_id, camera_make, img_d
 
     # Printing the summary information
     print(
-        f"\nEmail Received."
-        f"\nImages: {images_count}, Camera ID: {camera_id}, Camera Make: {camera_make}"
-        f"\nDate: {img_date}, Time: {img_time}, Temperature: {temp_deg_c}"
-        f"\nBattery: {battery}%, SD Memory: {sd_memory}%"
-        f"\nLocation: {location}, GPS: {gps}, Map URL: {map_url}"
+        f"\n{current_time()} | Email Received."
+        f"\n{current_time()} | Images: {images_count}, Camera ID: {camera_id}, Camera Make: {camera_make}"
+        f"\n{current_time()} | Date: {img_date}, Time: {img_time}, Temperature: {temp_deg_c}"
+        f"\n{current_time()} | Battery: {battery}%, SD Memory: {sd_memory}%"
+        f"\n{current_time()} | Location: {location}, GPS: {gps}, Map URL: {map_url}"
     )
 
     # Append the new data to the existing DataFrame
     df = pd.concat([df, new_data], ignore_index=True)
     
     return df
+
+def current_time():
+    now = datetime.now().strftime("%y-%m-%d %H:%M:%S")
+    return now
+
+
+##############################################################################
+############### ALERT CAPTION, ANNOTATE PICTURES, SEND ALERT #################
+##############################################################################
+
+
+from collections import Counter
+
+def generate_alert_caption(df, num_images, SPECIES_OF_INTEREST, EMAIL_USER, ALERT_LANGUAGE="en"):
+    
+    alert_caption = ""
+
+    # Summary variables
+    master_row = df.iloc[len(df) - num_images] # First row in the sequence
+    sequence_id = master_row['Sequence ID']
+    img_date = master_row['Date']
+    img_time = master_row['Time']
+    camera_id = master_row['Camera ID']
+    camera_make = master_row['Camera Make']
+    location = master_row['Location']
+    gps = master_row['GPS']
+    map_url = master_row['Map URL']
+    temperature = master_row['Temperature']
+    if temperature == "Unknown":
+        temperature = "See photo(s)"
+    battery = master_row['Battery']
+    sd_memory = master_row['SD Memory']
+
+    # Image specific variables, stored as lists
+    primary_species = df['Primary Species'].iloc[-num_images:].tolist()
+    animal_count = df['Animal Count'].iloc[-num_images:].tolist()
+    wild_boar_count = df['Wild Boar Count'].iloc[-num_images:].tolist()
+    bear_count = df['Bear Count'].iloc[-num_images:].tolist()
+    human_count = df['Human Count'].iloc[-num_images:].tolist()
+    vehicle_count = df['Vehicle Count'].iloc[-num_images:].tolist()
+    species_classes = df['Species Classes'].iloc[-num_images:].tolist()
+    species_confidences = df['Species Confidences'].iloc[-num_images:].tolist()
+
+    # Trigger a warning if humans or vehicles are detected in the image sequence
+    human_warning = any(count > 0 for count in human_count) or any(count > 0 for count in vehicle_count)        
+
+    # Determine if a priority alert should be sent (e.g. wild boar or bear detected)
+    priority_alert = None
+
+
+
+    for species_list in species_classes:
+        for species in SPECIES_OF_INTEREST:
+            if species in species_list:
+                priority_alert = species
+                break
+        if priority_alert:
+            break
+
+    if priority_alert == "Wild Boar":
+        priority_alert_count = max(wild_boar_count)
+    elif priority_alert == "Bear":
+        priority_alert_count = max(bear_count)
+
+
+
+    species_counts = Counter(primary_species)
+    max_count = max(species_counts.values())
+    most_common_species = [species for species, count in species_counts.items() if count == max_count]
+
+    if len(most_common_species) == 1:
+        sequence_primary_species = (most_common_species[0])
+    else:
+        sequence_primary_species = ", ".join(most_common_species)
+
+
+
+    unique_species = set(species for sublist in species_classes for species in sublist)
+    non_interest_species = unique_species - set(SPECIES_OF_INTEREST)
+
+    if not non_interest_species:
+        non_interest_species_str = "0"
+    else:
+        non_interest_species_str = ", ".join(non_interest_species) 
+
+
+
+
+    wild_boar_confidences = [conf for species_list, conf_list in zip(species_classes, species_confidences) for species, conf in zip(species_list, conf_list) if species == "Wild Boar"]
+    bear_confidences = [conf for species_list, conf_list in zip(species_classes, species_confidences) for species, conf in zip(species_list, conf_list) if species == "Bear"]
+    other_confidences = [conf for species_list, conf_list in zip(species_classes, species_confidences) for species, conf in zip(species_list, conf_list) if species in non_interest_species]
+
+    if wild_boar_confidences:
+        min_wild_boar_conf = round(min(wild_boar_confidences) * 100)
+        max_wild_boar_conf = round(max(wild_boar_confidences) * 100)
+        if min_wild_boar_conf == max_wild_boar_conf:
+            wild_boar_confidence_str = f"{min_wild_boar_conf}%"
+        else:
+            wild_boar_confidence_str = f"{min_wild_boar_conf}-{max_wild_boar_conf}%"
+    else:
+        wild_boar_confidence_str = "N/A"
+
+    if bear_confidences:
+        min_bear_conf = round(min(bear_confidences) * 100)
+        max_bear_conf = round(max(bear_confidences) * 100)
+        if min_bear_conf == max_bear_conf:
+            bear_confidence_str = f"{min_bear_conf}%"
+        else:
+            bear_confidence_str = f"{min_bear_conf}-{max_bear_conf}%"
+    else:
+        bear_confidence_str = "N/A"
+
+    if other_confidences:
+        min_other_conf = round(min(other_confidences) * 100)
+        max_other_conf = round(max(other_confidences) * 100)
+        if min_other_conf == max_other_conf:
+            other_confidence_str = f"{min_other_conf}%"
+        else:
+            other_confidence_str = f"{min_other_conf}-{max_other_conf}%"
+    else:
+        other_confidence_str = "N/A"
+
+
+
+
+
+    if priority_alert:
+        alert_caption = f":rotating_light: **{int(priority_alert_count)} {priority_alert.upper()} DETECTED** :rotating_light:"
+        print(f"{current_time()} | *** PRIORITY ALERT: AT LEAST {int(priority_alert_count)} {priority_alert.upper()} DETECTED IN IMAGE SEQUENCE***")
+
+        if human_warning:
+            alert_caption += f"WARNING: **{int(max(human_count))} HUMAN(S) ALSO DETECTED**"
+            print(f"{current_time()} | *** WARNING: {int(max(human_count))} HUMAN/VEHICLE(S) DETECTED IN IMAGE SEQUENCE ***")
+    elif human_warning:
+        alert_caption = f":person_walking_facing_right: **{int(max(human_count))} HUMAN(S) DETECTED** :horse_racing:"
+        print(f"{current_time()} | *** WARNING: {int(max(human_count))} HUMAN/VEHICLE(S) DETECTED IN IMAGE SEQUENCE ***")
+    else:
+        alert_caption = f":fox: **{int(max(animal_count))} {sequence_primary_species} Detected** :badger:"
+        print(f"{current_time()} | {int(max(animal_count))} Non-Priority Animal(s) Detected")
+
+    alert_caption += f"\n\nTime: {img_time}"
+    alert_caption += f"\nLocation: {location}"
+    alert_caption += f"\nGPS: {gps}"
+    alert_caption += f"\nMap Link: {map_url}"
+
+    if human_warning:
+        alert_caption += f"\n\n__Photos of humans are not sent between 06:00 and 21:00 to protect privacy. All photos are stored on {EMAIL_USER} for 48 hours before deletion.__"
+
+    alert_caption += f"\n\n**Detection Details**"
+    alert_caption += f"\nAnimals: {int(max(animal_count))}"
+    alert_caption += f"\nHumans: {int(max(human_count))}"
+    alert_caption += f"\nVehicles: {int(max(vehicle_count))}"
+    alert_caption += f"\nWild Boars: {int(max(wild_boar_count))} (Confidence: {wild_boar_confidence_str})"
+    alert_caption += f"\nBears: {int(max(bear_count))} (Confidence: {bear_confidence_str})"
+    alert_caption += f"\nOthers: {non_interest_species_str} (Confidence: {other_confidence_str})"
+    alert_caption += f"\nImage Sequence ID: {sequence_id}"
+
+    alert_caption += f"\n\n**Camera Details**"
+    alert_caption += f"\nCamera ID: {camera_id}"
+    alert_caption += f"\nCamera Brand: {camera_make}"
+    alert_caption += f"\nBattery Remaining: {battery}%"
+    alert_caption += f"\nStorage Remaining: {sd_memory}%"
+    alert_caption += f"\nCurrent Temperature: {temperature}"
+    alert_caption += f"\nDate: {img_date}"
+
+    flattened_alert_caption = alert_caption.replace('\n', '. ')
+    df.iloc[-num_images:, df.columns.get_loc('Alert Message')] = flattened_alert_caption
+
+
+    return df, alert_caption, human_warning, priority_alert
+
+##################################################
+############### HELPER FUNCTIONS #################
+##################################################
+
+
+def check_counts_and_species(df, images):
+    last_rows = df.iloc[-len(images):]
+    human_or_vehicle_present = (last_rows['Human Count'] > 0).any() or (last_rows['Vehicle Count'] > 0).any()
+    primary_species_not_empty = (last_rows['Primary Species'] != "Empty").any()
+    return human_or_vehicle_present or primary_species_not_empty
